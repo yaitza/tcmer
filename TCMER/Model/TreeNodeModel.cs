c5d8a7ba5d11962b785502d155e77c2a103b0e43
codeman <yaitza@foxmail.com>
2020-03-13 17:33:22 +0800

    树节点读取

--- TCMER/Model/TreeNodeModel.cs
+++ TCMER/Model/TreeNodeModel.cs
@@ -6,20 +6,49 @@ using System.Threading.Tasks;
 
 namespace TCMER.Model
 {
+    /// <summary>
+    /// 树节点
+    /// </summary>
     public class TreeNodeModel
     {
+        /// <summary>
+        /// 树节点ID
+        /// </summary>
         public String Id { get; set; }
 
+        /// <summary>
+        /// 树节点内容
+        /// </summary>
         public String DataBody { get; set; }
 
+        /// <summary>
+        /// 创建者
+        /// </summary>
         public String CreateBy { get; set; }
 
+        /// <summary>
+        /// 创建时间
+        /// </summary>
         public DateTime CreateTime { get; set; }
 
+        /// <summary>
+        /// 更新者
+        /// </summary>
         public String UpdateBy { get; set; }
 
+        /// <summary>
+        /// 更新时间
+        /// </summary>
         public DateTime UpdateTime { get; set; }
 
+        /// <summary>
+        /// 节点层级，顶级节点Depth=0，依次增加
+        /// </summary>
+        public int Depth { get; set; }
+
+        /// <summary>
+        /// 树节点下子节点
+        /// </summary>
         public List<TreeNodeModel> Children { get; set; }
     }
 }
---
+++ TCMer/Dao/TreeNodeMapper.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCMER.Model;
+using TCMER.Utils;
+
+namespace TCMER.Dao
+{
+    class TreeNodeMapper
+    {
+        private const string SqlStr = @"SELECT tn.ID,tn.DATA_BODY,tn.UPDATED_BY,tn.UPDATED_TIME,tn.CREATED_BY,tn.CREATED_TIME,th.DEPTH 
+                                FROM treenode tn LEFT JOIN treehierarchy th ON th.ANCESTOR = tn.ID AND th.DEPTH = {0};";
+
+        private const string SqlStr2 = @"SELECT tn.ID,tn.DATA_BODY,tn.UPDATED_BY,tn.UPDATED_TIME,tn.CREATED_BY,tn.CREATED_TIME,th.DEPTH
+                                FROM treenode tn LEFT JOIN treehierarchy th ON th.ANCESTOR = tn.ID AND th.DESCENDANT = {0};";
+
+        private readonly MySqlHelper _mySqlHelper;
+
+        public TreeNodeMapper()
+        {
+            _mySqlHelper = new MySqlHelper();
+        }
+
+        [Obsolete]
+        public List<TreeNodeModel> GetAllNodes()
+        {
+            const String queryStr =
+                @"SELECT tn.ID,tn.DATA_BODY,tn.UPDATED_BY,tn.UPDATED_TIME,tn.CREATED_BY,tn.CREATED_TIME,th.DEPTH FROM treenode tn LEFT JOIN treehierarchy th ON th.ANCESTOR = tn.ID AND th.DEPTH = 0;";
+            DataSet ds = _mySqlHelper.Query(queryStr);
+            List<TreeNodeModel> tnmList = new List<TreeNodeModel>();
+            foreach (DataTable dt in ds.Tables)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    TreeNodeModel tnm = new TreeNodeModel();
+                    tnm.Id = dr["ID"].ToString();
+                    tnm.DataBody = dr["DATA_BODY"].ToString();
+                    tnm.CreateBy = dr["CREATED_BY"].ToString();
+                    tnm.CreateTime = DateTime.Parse(dr["CREATED_TIME"].ToString());
+                    tnm.UpdateBy = dr["UPDATED_BY"].ToString();
+                    tnm.UpdateTime = DateTime.Parse(dr["UPDATED_TIME"].ToString());
+
+                    tnmList.Add(tnm);
+                }
+            }
+
+            return tnmList;
+        }
+
+        [Obsolete]
+        public List<TreeNodeModel> GetAllNodes(int depth)
+        {
+            List<TreeNodeModel> tnmList = new List<TreeNodeModel>();
+
+            string queryStr = string.Format(SqlStr, depth);
+            DataSet ds = _mySqlHelper.Query(queryStr);
+ 
+            foreach (DataTable dt in ds.Tables)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    TreeNodeModel tnm = new TreeNodeModel();
+                    tnm.Id = dr["ID"].ToString();
+                    tnm.DataBody = dr["DATA_BODY"].ToString();
+                    tnm.CreateBy = dr["CREATED_BY"].ToString();
+                    tnm.CreateTime = DateTime.Parse(dr["CREATED_TIME"].ToString());
+                    tnm.UpdateBy = dr["UPDATED_BY"].ToString();
+                    tnm.Depth = int.Parse(dr["DEPTH"].ToString());
+                    tnm.UpdateTime = DateTime.Parse(dr["UPDATED_TIME"].ToString());
+
+                    tnmList.Add(tnm);
+                }
+            }
+
+            return tnmList;
+        }
+
+        [Obsolete]
+        public List<TreeNodeModel> GetNodesByAncestor(String id)
+        {
+            List<TreeNodeModel> tnmList = new List<TreeNodeModel>();
+            string queryStr = string.Format(SqlStr2, id);
+            DataSet ds = _mySqlHelper.Query(queryStr);
+
+            foreach (DataTable dt in ds.Tables)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    TreeNodeModel tnm = new TreeNodeModel();
+                    tnm.Id = dr["ID"].ToString();
+                    tnm.DataBody = dr["DATA_BODY"].ToString();
+                    tnm.CreateBy = dr["CREATED_BY"].ToString();
+                    tnm.CreateTime = DateTime.Parse(dr["CREATED_TIME"].ToString());
+                    tnm.UpdateBy = dr["UPDATED_BY"].ToString();
+                    tnm.Depth = int.Parse(dr["DEPTH"].ToString());
+                    tnm.UpdateTime = DateTime.Parse(dr["UPDATED_TIME"].ToString());
+                    tnmList.Add(tnm);
+                }
+            }
+            return tnmList;
+        }
+
+
+
+    }
+}
